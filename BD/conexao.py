7aa252733281271352e3ca91cb235c3a42171f40
FLAVIO SANTARELLI <145938765+PROFSANTARELLI@users.noreply.github.com>
2025-04-25 21:27:32 -0300

    Update conexao.py

--- BD/conexao.py
+++ BD/conexao.py
@@ -3,7 +3,7 @@ import mysql.connector
 def conectar():
     return mysql.connector.connect(
         host="localhost",
-        user="root",
-        password="sua_senha",
+        user="aluno",
+        password="",
         database="escola"
     )
