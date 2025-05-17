8bc4776f508b8941839fb6bc63d078a78e2d600d
FLAVIO SANTARELLI <145938765+PROFSANTARELLI@users.noreply.github.com>
2025-05-17 10:08:03 -0300

    Update padaria_gui.py

--- PADARIA/PARTE-2/padaria_gui.py
+++ PADARIA/PARTE-2/padaria_gui.py
@@ -2,13 +2,13 @@ import tkinter as tk
 import subprocess
 
 def abrir_produtos():
-    subprocess.Popen(["python", "padaria_gui_produtos.py"])
+    subprocess.Popen(["python?", "padaria_gui_produtos.py"])
 
 def abrir_estoque():
-    subprocess.Popen(["python", "controle_estoque.py"])
+    subprocess.Popen(["python?", "controle_estoque.py"])
 
 def abrir_clientes():
-    subprocess.Popen(["python", "cadastro_clientes.py"])
+    subprocess.Popen(["python?", "cadastro_clientes.py"])
 
 janela = tk.Tk()
 janela.title("Sistema de Padaria - Menu Principal")
